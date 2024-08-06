1cc110e58eaa3a0882d40f647cc9cdfef672338b
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-08-06 18:05:27 +0400

    commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+#include "binary_trees.h"
+/**
+ * binary_tree_node - ...
+ * @parent: ...
+ * @value: ...
+ *
+ * Return: ...
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	return (node);
+}
