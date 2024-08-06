2639f43bceaeca8ff920a73e0d8431155ac49dc4
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-08-06 21:10:28 +0400

    is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,13 +7,13 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (!node)
-        {
-                return (0);
-        }
-        if (node->right || node->left)
-        {
-                return (0);
-        }
-        return (1);
+	if (!node)
+	{
+		return (0);
+	}
+	if (node->right || node->left)
+	{
+		return (0);
+	}
+	return (1);
 }
