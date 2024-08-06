e5705c758cdbeed26f951caafbd877c924ea58bc
RahmanKarimli <rahman.karimli@icloud.com>
2024-08-06 21:05:12 +0400

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,9 +7,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-  if (!node)
-  {
-    return (0);
+        if (!node)
+        {
+                return (0);
   }
   if (node->right || node->left)
   {
