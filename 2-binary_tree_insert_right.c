9681da2a8833d38009451c3ef39ac62603bee328
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-08-06 18:13:29 +0400

    commit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - ...
+ * @parent: ...
+ * @value: ...
+ *
+ * Return: ...
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+  binary_tree_t *tmp;
+
+  tmp = malloc(sizeof(binary_tree_t));
+  if (tmp == NULL)
+  {
+    return (NULL);
+  }
+  tmp->left = NULL;
+  tmp->n = value;
+  tmp->parent = parent;
+  tmp->right = value;
+  return (tmp);
+}
