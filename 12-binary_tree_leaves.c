637b182fc3a8200d21585af5bcd9da29541ae9fd
RahmanKarimli <rahman.karimli@icloud.com>
2024-08-07 20:45:18 +0400

    Update 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - ...
+ * @tree: ...
+ *
+ * Return: ...
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+  binary_tree_t *node;
+  size_t leaves = 0;
+
+  if (tree == NULL)
+  {
+    return (0);
+  }
+  node = tree;
+  if (node->left)
+  {
+    leaves += 1;
+  }
+  if (node->right)
+  {
+    leaves += 1;
+  }
+  else if (node->left == NULL && node->right == NULL)
+  {
+    leaves += 1;
+  }
+  return (leaves);
+}
