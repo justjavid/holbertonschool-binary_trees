ce74baeb5e1d400c7acadd0351e5bee634ad716e
RahmanKarimli <rahman.karimli@icloud.com>
2024-08-07 20:19:49 +0400

    Update 10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - ...
+ * @tree: ...
+ *
+ * Return: ...
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+  binary_tree_t *node;
+  size_t depth = 0;
+
+  if (tree == NULL)
+  {
+    return (0);
+  }
+  node = tree->parent;
+  while (node)
+  {
+    depth += 1;
+    node = node->parent;
+  }
+  return (depth);
+}
