3e1b4dc3640ac04421042cad69ccc24978737196
RahmanKarimli <rahman.karimli@icloud.com>
2024-08-07 10:59:41 +0400

    pre order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - ...
+ * @tree: ...
+ * @func: ...
+ *
+ * Return: ...
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+  if (tree == NULL || func == NULL)
+  {
+    return;
+  }
+  func(tree->n);
+  binary_tree_preorder(tree->left, *func);
+  binary_tree_preorder(tree->right, *func);
+}
