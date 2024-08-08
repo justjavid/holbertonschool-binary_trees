2f4e8b51865404c5367e1eaa8dc4f85132f5c374
RahmanKarimli <rahman.karimli@icloud.com>
2024-08-08 19:59:13 +0400

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,14 +12,12 @@
  * Return: Pointer to the uncle node, or NULL if no uncle exists.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
-        binary_tree_t *grandparent = node->parent->parent;
-    
+{    
     if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
         return (NULL);
 
-    if (grandparent->left == node->parent)
-        return (grandparent->right);
+    if (node->parent->parent->left == node->parent)
+        return (node->parent->parent->right);
     else
-        return (grandparent->left);
+        return (node->parent->parent->left);
 }
