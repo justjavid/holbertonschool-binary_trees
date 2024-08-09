ba3eb44feef1a349b45441f1ea560261c1fa9d19
root <you@example.com>
2024-08-09 12:27:15 +0000

    leaves

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,20 +12,20 @@
  */
 int height(binary_tree_t *node)
 {
-  int left, right;
-  
-  if (node == NULL)
-  {
-    return (-1);
-  }
-  left = height(node->left);
-  right = height(node->right);
+	int left, right;
 
-  if (left > right)
-  {
-    return left += 1;
-  }
-  return right += 1;
+	if (node == NULL)
+	{
+		return (-1);
+	}
+	left = height(node->left);
+	right = height(node->right);
+
+	if (left > right)
+	{
+		return left += 1;
+	}
+	return right += 1;
 }
 /**
  * binary_tree_balance - ...
@@ -35,13 +35,13 @@ int height(binary_tree_t *node)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  int left, right;
-  
-  if (tree == NULL)
-  {
-    return (0);
-  }
-  left = height(tree->left);
-  right = height(tree->right);
-  return (left - right);
+	int left, right;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left = height(tree->left);
+	right = height(tree->right);
+	return (left - right);
 }
