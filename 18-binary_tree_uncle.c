51391af6d762c922f56be52407db6e8516ac495f
root <you@example.com>
2024-08-09 12:38:41 +0000

    commit

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,7 @@
  * Return: Pointer to the uncle node, or NULL if no uncle exists.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{    
+{ 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
